20aff8fd346a5bfe842facae2c681f6ad5e57823
Kyle Greer <greer_gregory@students.columbusstate.edu>
2026-01-22 14:57:14 -0500

    Add files via upload

---
+++ First/src/First.java
@@ -0,0 +1,9 @@
+// Kyle Greer
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Hola, Mundo!");
+        System.out.println("Title: Graphic Designer");
+        System.out.println("Why? Love creating and designing");
+        System.out.println("Median Salary: $61,300 per year");
+    }
+}
