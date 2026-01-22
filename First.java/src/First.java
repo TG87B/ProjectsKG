f7e50e9535beeb8517990a80f8436c4acea1719c
Kyle <greer_gregory@students.columbusstate.edu>
2026-01-22 13:09:50 -0500

    Our first commit, Assignment 1

---
+++ First.java/src/First.java
@@ -0,0 +1,8 @@
+public class First {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hola, Mundo!");
+        System.out.println("Title: Graphic Designer");
+        System.out.println("Why? Love creating and designing");
+        System.out.println("Median Salary: $61,300 per year");
+    }
+}
