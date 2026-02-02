539440dede6a8a548c6cde99929bb7df8d07464d
Kyle Greer <greer_gregory@students.columbusstate.edu>
2026-02-02 11:26:36 -0500

    Add files via upload

---
+++ tests.java
@@ -0,0 +1,16 @@
+public class tests {
+    public static void main(String[] args) {
+
+        double test1 = 75.6;
+        double test2 = 63.7;
+        double test3 = 93.8;
+        double average = (test1 + test2 + test3) / 3;
+        
+        System.out.println ("Test Score 1: " + test1 );
+        System.out.println ("Test Score 2: " + test2 );
+        System.out.println ("Test Score 3: " + test3 );
+        System.out.println ("The average of all Test Scores: " + average);
+
+
+    }
+}
