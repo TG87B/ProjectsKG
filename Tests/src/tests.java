c5b20fdb2d371bff5f3c7adce6ba83bc4fe72ccf
Kyle Greer <greer_gregory@students.columbusstate.edu>
2026-02-02 11:30:32 -0500

    Update tests.java

--- Tests/src/tests.java
+++ Tests/src/tests.java
@@ -1,11 +1,14 @@
+//Kyle Greer
+
 public class tests {
     public static void main(String[] args) {
 
+     //Scores and averages
         double test1 = 75.6;
         double test2 = 63.7;
         double test3 = 93.8;
         double average = (test1 + test2 + test3) / 3;
-        
+     //Prints  
         System.out.println ("Test Score 1: " + test1 );
         System.out.println ("Test Score 2: " + test2 );
         System.out.println ("Test Score 3: " + test3 );
@@ -14,3 +17,4 @@ public class tests {
 
     }
 }
+
